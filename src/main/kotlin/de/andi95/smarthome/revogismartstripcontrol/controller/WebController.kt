b29cad606482c6beba4dca670ea145e61402e5df
Andreas Bräu <ab@andi95.de>
2018-08-13 22:49:22 +0200

    sent first udp package successfully

--- src/main/kotlin/de/andi95/smarthome/revogismartstripcontrol/controller/WebController.kt
+++ src/main/kotlin/de/andi95/smarthome/revogismartstripcontrol/controller/WebController.kt
@@ -1,15 +1,23 @@
 package de.andi95.smarthome.revogismartstripcontrol.controller
 
+import de.andi95.smarthome.revogismartstripcontrol.service.UdpSenderService
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
-class WebController
+class WebController(@Autowired private val udpSenderService: UdpSenderService) {
+
+    val log = LoggerFactory.getLogger(WebController::class.java)!!
+
 
     @GetMapping("/")
     fun indexController(model: Model): String {
         model["title"] = "hello"
+        log.info(udpSenderService.sendUpdDatagram("00sw=all,2018-08-06,17:32:15,10"))
         return "index"
     }
+}
--- src/main/kotlin/de/andi95/smarthome/revogismartstripcontrol/service/UdpSenderService.kt
+++ src/main/kotlin/de/andi95/smarthome/revogismartstripcontrol/service/UdpSenderService.kt
@@ -1,19 +1,40 @@
 package de.andi95.smarthome.revogismartstripcontrol.service
 
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import java.net.DatagramPacket
 import java.net.DatagramSocket
+import java.net.NetworkInterface
 import java.nio.charset.Charset
+import java.util.stream.Collectors
 
 @Service
 class UdpSenderService {
 
+    val log = LoggerFactory.getLogger(UdpSenderService::class.java)!!
+
     val socket = DatagramSocket()
+
+    private val revogiPort = 8888
 
     fun sendUpdDatagram(content: String): String {
         val buf = content.toByteArray(Charset.defaultCharset())
-        val packet = DatagramPacket(buf, buf.size)
+        log.info(NetworkInterface.getNetworkInterfaces().toString())
+        val broadcastAddresses = NetworkInterface.getNetworkInterfaces().toList().stream()
+                .filter { i -> i.isUp }
+                .flatMap { iface -> iface.interfaceAddresses.stream() }
+                .filter { a -> a.broadcast != null }
+                .map { a -> a.broadcast }
+                .collect(Collectors.toList())
+        log.info("Using address {}", broadcastAddresses.first())
+        val packet = DatagramPacket(buf, buf.size, broadcastAddresses.first(), revogiPort)
+        socket.broadcast = true
+        socket.soTimeout = 2
         socket.send(packet)
+        val receivedBuf = ByteArray(100)
+        val answer = DatagramPacket(receivedBuf, receivedBuf.size)
+        socket.receive(answer)
+        return String(answer.data, 0, answer.length)
     }
 
 }
