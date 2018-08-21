714275e6dc30ba60555e2cdfa491664724d4ca0f
Andreas Bräu <ab@andi95.de>
2018-08-21 09:09:59 +0200

    more meaningful test

--- src/test/kotlin/de/andi95/smarthome/revogismartstripcontrol/service/UdpSenderServiceTest.kt
+++ src/test/kotlin/de/andi95/smarthome/revogismartstripcontrol/service/UdpSenderServiceTest.kt
@@ -1,15 +1,53 @@
 package de.andi95.smarthome.revogismartstripcontrol.service
 
+import jdk.nashorn.internal.ir.annotations.Ignore
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.mockito.ArgumentMatchers.any
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.times
+import org.mockito.Mockito.verify
+import org.springframework.test.util.ReflectionTestUtils
+import java.net.DatagramPacket
+import java.net.DatagramSocket
+import java.net.SocketTimeoutException
 
 class UdpSenderServiceTest {
 
     private var udpSenderService: UdpSenderService = UdpSenderService()
 
+    private var datagramSocket = mock(DatagramSocket::class.java)
+
     @Test
-    fun indexController() {
-        assertThat(udpSenderService.broadcastUpdDatagram("We expect no response here")).isEmpty()
+    fun testTimeout() {
+        // given
+        ReflectionTestUtils.setField(udpSenderService, "socket", datagramSocket)
+        `when`(datagramSocket.receive(any())).thenThrow(SocketTimeoutException::class.java)
+
+        // when
+        val list = udpSenderService.broadcastUpdDatagram("send something")
+
+        // then
+        assertThat(list).isEmpty()
+        verify(datagramSocket, times(3)).receive(any())
+    }
+
+    @Test
+    @Ignore
+    fun testOneAnswer() {
+        // given
+        val receivedBuf = "test".toByteArray()
+        val answer = DatagramPacket(receivedBuf, receivedBuf.size)
+        ReflectionTestUtils.setField(udpSenderService, "socket", datagramSocket)
+        `when`(datagramSocket.receive(any())).thenThrow(SocketTimeoutException::class.java)
+
+        // when
+        val list = udpSenderService.broadcastUpdDatagram("send something")
+
+        // then
+        assertThat(list).isEmpty()
+        verify(datagramSocket, times(3)).receive(any())
     }
 
 }
